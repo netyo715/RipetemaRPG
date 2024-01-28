c26c6ec73a477687357fc1a245f479cfb0058bf0
Sohei Nishimura <nishimura715@gmail.com>
2024-01-29 00:48:56 +0900

    戦闘関連UI改善

--- src/components/screens/battleSection/BattleView.tsx
+++ src/components/screens/battleSection/BattleView.tsx
@@ -1,18 +1,29 @@
-import { Button, VStack } from "@yamada-ui/react";
+import {
+  CloseButton,
+  HStack,
+  ScrollArea,
+  Spacer,
+  VStack,
+} from "@yamada-ui/react";
 import { useBattleManager } from "../../../contexts/BattleManagerProvider";
 
 export const BattleView: React.FC = () => {
   const { startBattle, closeBattle, battleLog } = useBattleManager();
   return (
-    <VStack h="full" w="full">
-      <Button minH="100px" onClick={closeBattle}>
-        close
-      </Button>
-      <VStack>
+    <VStack h="full">
+      <HStack>
+        <Spacer />
+        <CloseButton onClick={closeBattle} />
+      </HStack>
+      <ScrollArea
+        p="md"
+        type="always"
+        innerProps={{ as: VStack, minH: 0, gap: 0 }}
+      >
         {battleLog.map((text, index) => (
           <p key={index}>{text}</p>
         ))}
-      </VStack>
+      </ScrollArea>
     </VStack>
   );
 };
--- src/components/screens/battleSection/Map.tsx
+++ src/components/screens/battleSection/Map.tsx
@@ -1,25 +1,37 @@
-import { Button, Heading, VStack } from "@yamada-ui/react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionPanel,
+  Button,
+  Heading,
+  VStack,
+} from "@yamada-ui/react";
 import { useBattleManager } from "../../../contexts/BattleManagerProvider";
 import { DUNGEONS } from "../../../data/dungeon";
 
 export const Map: React.FC = () => {
   const { startBattle, closeBattle, battleLog } = useBattleManager();
   return (
-    <VStack h="full" w="full">
-      {DUNGEONS.map((dungeon) => {
-        return (
-          <VStack>
-            <Heading>{dungeon.name}</Heading>
-            <VStack>
-              {dungeon.areas.map((area) => {
-                return (
-                  <Button onClick={() => startBattle(area)}>{area.name}</Button>
-                );
-              })}
-            </VStack>
-          </VStack>
-        );
-      })}
+    <VStack>
+      <Accordion isMultiple>
+        {DUNGEONS.map((dungeon) => {
+          return (
+            <AccordionItem label={dungeon.name}>
+              <AccordionPanel p="sm">
+                <VStack gap="sm">
+                  {dungeon.areas.map((area) => {
+                    return (
+                      <Button variant="ghost" justifyContent="start" onClick={() => startBattle(area)}>
+                        {area.name}
+                      </Button>
+                    );
+                  })}
+                </VStack>
+              </AccordionPanel>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
     </VStack>
   );
 };
